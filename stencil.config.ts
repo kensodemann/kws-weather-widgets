cb4fbf339d8d008b76249c281b91c857724f8055
Ken Sodemann <ken@ionic.io>
2018-07-31 18:10:05 -0500

    chore(lib): upgrade Stencil to 0.11.0

---
+++ stencil.config.ts
@@ -0,0 +1,13 @@
+import { Config } from '@stencil/core';
+
+export const config:Config = {
+  namespace: 'kwsweather',
+  outputTargets:[
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www'
+    }
+  ]
+};
